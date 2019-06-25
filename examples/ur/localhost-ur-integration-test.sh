2befcd383ed68aa9285c320321cfb81ae9541820
pferrel <pat@occamsmachete.com>
2019-06-24 17:44:00 -0700

    we have all of localhost-ur-integration-test.sh running with harness 0.4.0

--- examples/ur/localhost-ur-integration-test.sh
+++ examples/ur/localhost-ur-integration-test.sh
@@ -92,7 +92,7 @@ echo
 ./${test_queries} ${host_url} > ${actual_query_results}
 
 #END
-: <<'END' # block comment beginning look for END
+#: <<'END' # block comment beginning look for END
 
 echo
 echo "---------------------- Testing Event Aliases -------------------------------------------------------------------"
@@ -123,11 +123,11 @@ echo "Sending UR queries"
 echo
 ./${test_queries} ${host_url} > ${actual_query_results_aliases}
 
-END
+#END
+#: <<'END' # block comment beginning look for END
 
 echo "---------------------- Testing Queries Filtered by Dates -------------------------------------------------------"
 
-#: <<'END' # block comment beginning look for END
 
 engine_dates_json=examples/ur/localhost_test_ur_mobile_device_dates.json
 user_events_dates=examples/ur/sample-mobile-device-ur-data.csv
@@ -165,16 +165,15 @@ set +e # ignore trivial errors like no grep match
 
 rm -f ${diffs_and_errors_file}
 
-#echo "default" >> ${diffs_and_errors_file}
+##echo "default" >> ${diffs_and_errors_file}
 diff ${actual_query_results} ${expected_test_results} | grep "result" >> ${diffs_and_errors_file}
 cat ${actual_query_results} | grep "error" >> ${diffs_and_errors_file}
-#echo "aliases" >> ${diffs_and_errors_file}
 
-# disable alias queries, which DO NOT match regular indicators
-#diff ${actual_query_results_aliases} ${expected_test_results} | grep "result" >> ${diffs_and_errors_file}
-#cat ${actual_query_results_aliases} | grep "error"  >> ${diffs_and_errors_file}
+##echo "aliases" >> ${diffs_and_errors_file}
+diff ${actual_query_results_aliases} ${expected_test_results} | grep "result" >> ${diffs_and_errors_file}
+cat ${actual_query_results_aliases} | grep "error"  >> ${diffs_and_errors_file}
 
-#echo "date filters" >> ${diffs_and_errors_file}
+##echo "date filters" >> ${diffs_and_errors_file}
 diff ${actual_query_results_dates} ${expected_test_results_dates} | grep "result"  >> ${diffs_and_errors_file}
 cat ${actual_query_results_dates} | grep "error" >> ${diffs_and_errors_file}
 
