bbfc9a4ffb1b82c1c273a5c561c5390a2ea27ad9
pferrel <pat@occamsmachete.com>
2020-08-08 14:19:28 -0700

    examples/query.sh

--- examples/query.sh
+++ examples/query.sh
@@ -15,5 +15,91 @@ curl -H "Content-Type: application/json" -d '
 echo
 curl -H "Content-Type: application/json" -d '
 {
-  "itemSet": ["Galaxy 8", "Galaxy 8"]
+  "itemSet": ["Galaxy 8", "iPhone 8"]
+}' http://localhost:9090/engines/test_ur/queries
+
+#!/usr/bin/env bash
+
+echo
+curl -H "Content-Type: application/json" -d '
+{
+  "item": "Galaxy 8",
+  "rules": [
+    {
+      "name": "categories",
+      "values": ["Phones"],
+      "bias": -1
+    }
+  ]
+}' http://localhost:9090/engines/test_ur/queries
+
+echo
+curl -H "Content-Type: application/json" -d '
+{
+  "itemSet": ["Galaxy 8"],
+  "rules": [
+    {
+      "name": "categories",
+      "values": ["Phones"],
+      "bias": -1
+    }
+  ]
+}' http://localhost:9090/engines/test_ur/queries
+
+echo
+curl -H "Content-Type: application/json" -d '
+{
+  "itemSet": ["Galaxy 8", "iPhone 8"],
+  "rules": [
+    {
+      "name": "categories",
+      "values": ["Phones"],
+      "bias": -1
+    }
+  ]
+}' http://localhost:9090/engines/test_ur/queries
+
+#!/usr/bin/env bash
+
+echo
+curl -H "Content-Type: application/json" -d '
+{
+  "item": "Galaxy 8",
+  "rules": [
+    {
+      "name": "categories",
+      "values": ["Apple"],
+      "bias": 0
+    }
+  ]
+}' http://localhost:9090/engines/test_ur/queries
+
+echo
+curl -H "Content-Type: application/json" -d '
+{
+  "itemSet": ["Galaxy 8"],
+  "rules": [
+    {
+      "name": "categories",
+      "values": ["Apple"],
+      "bias": 0
+    }
+  ]
+}' http://localhost:9090/engines/test_ur/queries
+
+echo
+curl -H "Content-Type: application/json" -d '
+{
+  "user": "",
+  "rules": [
+    {
+      "name": "categories",
+      "values": ["Apple"],
+      "bias": 0
+    }
+  ]
+}' http://localhost:9090/engines/test_ur/queries
+
+curl -H "Content-Type: application/json" -d '
+{
 }' http://localhost:9090/engines/test_ur/queries
