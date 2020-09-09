6edd1e0defbc070e23a642ff07eac1eeb5213f12
pferrel <pat@occamsmachete.com>
2020-09-09 10:39:37 -0700

    cleanup ad-hoc query scripts

--- query.sh
+++ query.sh
@@ -1,34 +1,8 @@
 #!/usr/bin/env bash
 curl -H "Content-Type: application/json" -d '
 {
-    "from": 0,
-    "num": 2,
-    "rules": [
-    {
-        "name": "opportunity-class",
-        "values": ["FederalContractOpportunityModel"],
-        "bias": -1
-    },
-    {
-        "name": "set-aside",
-        "values": ["No Set-Aside Used"],
-        "bias": -1
-    },
-    {
-        "name": "naics",
-        "values": ["511210-N", "541519-N"],
-        "bias": -1
-    },
-    {
-        "name": "opp-type",
-        "values": ["Solicitation", "Pre-Solicitation", "Special Notice", "Grant", "Cooperative Agreement", "Procurement Contract", "Other"],
-        "bias": -1
-    }],
-    "dateRange":
-    {
-        "name": "due-date",
-        "after": "2020-04-24T21:07:06Z"
-    },
-    "user": "YWFXNE5xYVhicmx3NkVsUXhnRm5yUT09"
-}' http://gt:9090/engines/govtribe_opportunity_recommender_v7/queries
-echo
+   "event":"$delete",
+   "entityType":"item",
+   "entityId":"iPhone XS",
+   "eventTime" : "2016-10-05T21:02:49.228Z"
+}' http://localhost:9090/engines/test_ur/events
